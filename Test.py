25ca9d7cfbc0b41f1d06911a06590e4e8b5d0a85
xuejun <xuejunzhao0@gmail.com>
2023-05-17 15:26:49 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,11 +1,12 @@
 
 from build import libadd_
 
-obj = libadd_.CT(1,3,[4,5])         #初始化对象
+#枚举类型访问
 enum = libadd_.Type
-print(enum(0).name,enum(1).value)     #枚举类型访问
+print(enum(0).name,enum(1).value)
 
-# print(obj.Type)
+#初始化对象
+obj = libadd_.CT(1,3,[4,5])
 sum =obj.add()          #调用普通成员函数
 obj.print()             #调用访问STL vector的函数
 obj.a=300               #可变值重新v值
