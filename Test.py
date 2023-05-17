7b04f7c9e9c8ffc57233cb66c8987e1d37a06be2
xuejun <xuejunzhao0@gmail.com>
2023-05-17 15:24:26 +0800

    Update Lib.cpp

--- Test.py
+++ Test.py
@@ -1,6 +1,13 @@
 
 from build import libadd_
 
-c = libadd_.CT(1,3)
-m =c.add()
-print("add result result is",m)
+obj = libadd_.CT(1,3,[4,5])         #初始化对象
+enum = libadd_.Type
+print(enum(0).name,enum(1).value)     #枚举类型访问
+
+# print(obj.Type)
+sum =obj.add()          #调用普通成员函数
+obj.print()             #调用访问STL vector的函数
+obj.a=300               #可变值重新v值
+print("c为{}，d为{}".format(obj.a,obj.d))
+print("add result result is",sum)
--- _lib.cpp
+++ _lib.cpp
@@ -2,35 +2,65 @@
 // Created by xuejun on 23-5-11.
 //
 #include <iostream>
+#include <vector>
+#include <pybind11/stl.h>
 #include <pybind11/pybind11.h>
-//#include<pybind11/detail/common.h>
 using namespace std;
 namespace py = pybind11;
 class Base
 {
 public:
-    Base(int a, int b):a(a),b(b){}
+    Base(int a, int b,vector<int> c):a(a),b(b),v(c){}
     int add(){cout<<"add a and b"<<endl;return a+b;}
+    void print()
+    {
+        for(auto n:v)
+        {
+            cout<<"vector element is "<<n<<" "<<flush;
+            cout<<endl;
+        }
+    }
+
+    int c = 10;
+    int d = 20;
 
 private:
     int a;
     int b;
+    vector<int> v;
+
 };
 int add_1(int a, int b)
 {
     cout<<"fun has been called"<<endl;
     return a+b;
 }
+enum class Type:char {Sheep,Bat, Lion};
+//PYBIND11_MODULE(enum_,m){
+//    m.doc() = "enum example";
+//    py::enum_<Type>(m,"Type")
+//        .value("Sheep",Type::Sheep)
+//        .value("Lion",Type::Lion)
+//        .value("Bat",Type::Bat)
+//        .export_values();
+//}
 //普通函数封装为动态库
 //PYBIND11_MODULE(libadd, m){
 //    m.doc() = "pybind11 example";
 //    m.def("add", &add_1, "add two number",py::arg("a")=1,py::arg("b")=2);
 //}
 //类及函数封装为动态库
-PYBIND11_MODULE(libadd_, m){     //(模块名，实例化对象)
-    py::class_<Base>(m,"CT")    //"CT"为实例化的对象
-        .def(py::init<int ,int >())
-        .def("add",&Base::add);  //(目标函数名，引用的实际函数)
+PYBIND11_MODULE(libadd_, m) {     //(模块名，实例化对象)
+    py::class_<Base>(m, "CT")    //"CT"为实例化的对象
+            .def(py::init<int, int,vector<int>>())
+            .def("add", &Base::add)  //(目标函数名，引用的实际函数)
+            .def("print",&Base::print)
+            .def_readwrite("a",&Base::c)
+            .def_readonly("d",&Base::d);
+    py::enum_<Type>(m,"Type")
+            .value("Sheep",Type::Sheep)
+            .value("Lion",Type::Lion)
+            .value("Bat",Type::Bat);
 }
 
 //
